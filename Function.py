51e2a6acd21f185d9a7d2e834d1f018a671ab18e
MatteCaraff <102987352+MatteCaraff@users.noreply.github.com>
2022-04-04 21:49:23 +0200

    Assestamento file
    ho sistemato e commentato a dovere più o meno tutti i file e riordinati da ora si può lavorare su del materiale decente

--- Function.py
+++ Function.py
@@ -1,3 +1,6 @@
+from sympy import *     # introdotti solo per non scriverlo nel main preferisco usare la sintassi con il punto #
+from numpy import *     # per saperer da dove arriva il metodo #
+
 import sympy
 import numpy
 
@@ -7,7 +10,7 @@ import numpy
 def mat(function_simbolic, parameter):
         return sympy.lambdify (parameter, function_simbolic, modules=['numpy']) #funzione con in formato matematico e non piu simbolico
 
-#  CALCOLO NUMERICO DI UNA FUNZIONE #
+# CALCOLO NUMERICO DI UNA FUNZIONE #
 # claclolo la funzione in un punto utilizzando il foramto numpy #
 def calc(function_simbolic, parameter, vaule):
         function_mat = mat(function_simbolic, parameter)
@@ -18,3 +21,7 @@ def calc(function_simbolic, parameter, start, end, interval):
         function_mat = mat(function_simbolic, parameter)
         a = numpy.linspace(start, end, interval)
         return function_mat(a)
+
+def calc_interval(function_simbolic, parameter, interval):               #duale alla precedente ma si passa direttamente l'intervallo sotto forma di lista della numpy
+        function_mat = mat(function_simbolic, parameter)                 #vorrei usare lo stesso nome della funzione come per una classe è possibile?
+        return function_mat(interval)
--- Test_esportazione_tabella_pdf.py
+++ Test_esportazione_tabella_pdf.py
@@ -1,35 +1,32 @@
 ##  importo libbrerie e funzioni che mi servono ##
-import numpy as np                     #serve per generare le liste per gli intervalli
-import matplotlib.pyplot as plt        #serve per plottare
-from sympy import*
-import sympy.printing as platex
+from Function import *
 import pandas as pd
-import pdfkit
+
+import sympy.printing as platex        #serve per esportare il latex le funzioni dell sympy
+import pdfkit                          #esportazione in file html e in file pdf
+
 
 # definisco alcune funzioni #
 def p_latex(a):
-    return '$' + platex.latex(a) + '$'  #restituisce in scrittura latex a i caratteri '$' servono per l'interpretazione in latex 
+    return '$' + platex.latex(a) + '$'      #restituisce in scrittura latex a i caratteri '$' servono per l'interpretazione in latex 
 
-##  definisco funzioni e variabili simboliche  ##
+## definisco funzioni e variabili simboliche ##
 x = symbols('x')
 f = 0.05*x + 0.2/((x - 5)**2 + 2)
 
-## converto le funzioni simboliche in funzioni matematiche  ##
-## in questo modo le posso calcolare in un'intervalo        ##
-lam_f = lambdify(x, f, modules=['numpy'])   #converte la  funzione simbolica in funzione matematica
-
-##  calcolo le funzioni in un'intervallo ##
-x_plot = np.linspace(0, 10, 10)
-f_plot = lam_f(x_plot)
-
-#  scrive in un file html ed esporta in un pdf #
-df = {'x_plot': x_plot, 'f_plot': f_plot}
+## calcolo le funzioni in un'intervallo e le inserisco in ##
+## dataframe con il modulo pandas ##
+intervallo = linspace(0, 10, 10)
+f_intervallo = calc(f, x, 0, 10, 10)
+#f_intervallo = calc_interval(f, x, intervallo)              #guarda nota file Function
+df = {'x': intervallo, 'f': f_intervallo}
 df = pd.DataFrame(data=df)
-f = open('exp.html','w')
+
+
+# scrive in un file html ed esporta in un pdf #
+f = open('exp.html','w')            #inizio file htlm
 a = df.to_html()
 f.write(a)
-f.close()
+f.close()                           #fine file html
 
-pdfkit.from_file('exp.html', 'example.pdf')     #NON VA
-
-plt.show()                                     #mostra tutti i plot fatti
+pdfkit.from_file('exp.html', 'example.pdf')     #NON VA dovrebbe esportare l'html in un pdf
